0e7895c9d28d469430132b094087e1688af24d4b
yangyue <daisysong19900215@gmail.com>
2022-09-13 10:48:23 -0700

    Feature 11 complete

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    start_date = models.DateTimeField()
+    due_date = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    project = models.ForeignKey(
+        "projects.Project",
+        related_name="tasks",
+        on_delete=models.CASCADE)
+    assignee = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        related_name="tasks",
+        on_delete=models.SET_NULL,
+    )
+
+    def __str__(self):
+        return self.name
