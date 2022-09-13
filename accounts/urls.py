f0511ee5f3aa7b5533342c37d0b6c310df999d1e
yangyue <daisysong19900215@gmail.com>
2022-09-13 10:26:54 -0700

    Feature 9 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
+from .views import signup
 
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", signup, name="signup")
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
-from .models import Project
+from projects.models import Project
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 
