fd55168e1ddcd677ffb8d18164d46b219ce09948
yangyue <daisysong19900215@gmail.com>
2022-09-14 17:10:46 -0700

    Feature 17 complete

--- tasks/models.py
+++ tasks/models.py
@@ -4,8 +4,8 @@ from django.conf import settings
 
 class Task(models.Model):
     name = models.CharField(max_length=200)
-    start_date = models.DateTimeField()
-    due_date = models.DateTimeField()
+    start_date = models.DateField()
+    due_date = models.DateField()
     is_completed = models.BooleanField(default=False)
     project = models.ForeignKey(
         "projects.Project",
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 from django.urls import reverse_lazy
+from django.views.generic.list import ListView
 
 
 class TaskCreateView(LoginRequiredMixin, CreateView):
@@ -11,21 +12,21 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
     fields = ["name", "start_date", "due_date", "project", "assignee"]
 
     def get_success_url(self):
-        return reverse_lazy("show_project", args=[self.object.id])
+        return reverse_lazy("show_project", args=[self.object.project.id])
 
 
-class TaskListView(LoginRequiredMixin, CreateView):
+class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = "tasks/list.html"
-    fields = ["name", "start_date", "due_date", "project", "assignee"]
+    fields = ["name", "start_date", "due_date", "is_completed"]
 
     def get_queryset(self):
         return Task.objects.filter(assignee=self.request.user)
 
 
-class TaskUpdateView(LoginRequiredMixin, CreateView):
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
-    template_name = "tasks/edit.html"
+    template_name = "tasks/list.html"
     fields = ["is_completed"]
 
     def get_success_url(self):
